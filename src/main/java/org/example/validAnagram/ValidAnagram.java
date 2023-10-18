2f22aaadc5900434bb879d259f9e73a19c35fb1b
murtaizinram <murtaizinram@gmail.com>
2023-10-18 14:52:44 +0200

    add ValidAnagram

---
+++ src/main/java/org/example/validAnagram/ValidAnagram.java
@@ -0,0 +1,40 @@
+package org.example.validAnagram;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * https://leetcode.com/problems/valid-anagram/
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ *
+ * Example 1:
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ *
+ * Example 2:
+ * Input: s = "rat", t = "car"
+ * Output: false
+ *
+ * Constraints:
+ * 1 <= s.length, t.length <= 5 * 104
+ * s and t consist of lowercase English letters.
+ */
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        int compare = Arrays.compare(s1, t1);
+        return compare == 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
