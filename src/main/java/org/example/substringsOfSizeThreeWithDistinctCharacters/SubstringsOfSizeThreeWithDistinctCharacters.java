1444d9a10f4894ed7f1ae5a6b2e0e95668c40ee7
murtaizinram <murtaizinram@gmail.com>
2023-10-25 11:45:07 +0200

    add SubstringsOfSizeThreeWithDistinctCharacters

--- src/main/java/org/example/substringsOfSizeThreeWithDistinctCharacters/SubstringsOfSizeThreeWithDistinctCharacters.java
+++ src/main/java/org/example/substringsOfSizeThreeWithDistinctCharacters/SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -1,7 +1,13 @@
 package org.example.substringsOfSizeThreeWithDistinctCharacters;
 
 import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntFunction;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
@@ -39,6 +45,31 @@ public class SubstringsOfSizeThreeWithDistinctCharacters {
     }
 
     public static void main(String[] args) {
-
+        String s = "Welcome";
+        String s1 = spinWords(s);
+        System.out.println(s1);
     }
+
+    public static String spinWords(String sentence) {
+        String[] arr = sentence.split(" ");
+        String collect = Arrays.stream(arr)
+                .map(StringBuilder::new)
+                .map(f -> f.reverse().toString())
+                .collect(Collectors.joining(""));
+        return collect;
+    }
+
+    public static int[] arrayDiff(int[] a, int[] b) {
+        //1 2 2
+        List<Integer> collect1 = IntStream.of(a).boxed().collect(Collectors.toList());
+        List<Integer> collect2 = IntStream.of(b).boxed().collect(Collectors.toList());
+        int[] ints = collect1.stream().filter(p -> !collect2.contains(p)).mapToInt(value -> value).toArray();
+        return ints;
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+        String str = Arrays.stream(numbers).mapToObj(String::valueOf).collect(Collectors.joining(""));
+        return "(" + str.substring(0, 3) + ") " + str.substring(4, 7) + "-" + str.substring(8, str.length() - 1);
+    }
+
 }
